0b94327c82467005b4a32f0f731b23d741fc40b2
Otis Singfield <singfieldotis@gmail.com>
2021-05-16 19:44:50 -0400

    set up table schema according to diagram

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,19 @@ SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department
     INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
     INNER JOIN department ON (department.id = role.department_id)
     ORDER BY manager;
+
+SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY role.title;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;
+
+SELECT first_name, last_name, role_id FROM employee WHERE employee.id = 4;
+
